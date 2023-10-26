f6b5f23bf4a3435a2b322a580c3a3bc267c9f0a5
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-26 17:30:15 +0500

    custom namespace in socket io

--- app.js
+++ app.js
@@ -2,76 +2,103 @@ const app = require('express')();
 const http = require('http').Server(app);
 const path = require('path');
 const io = require('socket.io')(http);
-const PORT = 5000
+const PORT = 5000;
 
-// routes
+// Serve the index.html file
 app.get('/', function (req, res) {
     const options = {
         root: path.join(__dirname),
-    }
+    };
     let fileName = 'index.html';
     res.sendFile(fileName, options);
-})
+});
 
-// count of total users
-let users = 0
+// Serve the index.html file for the custom-namespace path
+app.get('/custom-namespace', function (req, res) {
+    const options = {
+        root: path.join(__dirname),
+    };
+    let fileName = 'index.html';
+    res.sendFile(fileName, options);
+});
+// Count of total users
+let users = 0;
 
-// #### connection established ######
+
+
+// Connection established
 io.on('connection', function (socket) {
-    console.log(`A user connected`)
-    // incrementing the user count
+    console.log(`A user connected`);
+    // Incrementing the user count
     users++;
-    // broadcasting the message including total users
-    // io.sockets.emit('broadcast', {
-    //     message: `${users} users connected`
-    // })
 
-    // show welcome message to connected user
+    // Show a welcome message to the connected user
     socket.emit('newUserConnect', {
         message: `Hi, Welcome to the community of ${users} users`
-    })
+    });
 
-    // and broadcast the message to those users who were already connected
+    // Broadcast the message to those users who were already connected
     socket.broadcast.emit('newUserConnect', {
         message: `${users} connected`
-    })
+    });
 
+    // Sending a message from the server side
+    socket.send(`Sent message from the server side by pre-reserved events`);
 
-    // message event
+    // Create a custom event on the server side
     setTimeout(function () {
-        socket.send(`Sent message from server side by prereserved events`)
-        // create custom event on server side
-        setTimeout(function () {
-            socket.emit('myCustomEvent', {
-                description: 'A custom message from server side custom event'
-            })
-        }, 4000)
-    }, 3000);
-    // method 2 - catching custom client side event
-    socket.on('myCustomEventFromClientSide', function(data){
+        socket.emit('myCustomEvent', {
+            description: 'A custom message from the server side custom event'
+        });
+    }, 4000);
+
+    // Method 2: Catching custom client-side event
+    socket.on('myCustomEventFromClientSide', function (data) {
         console.log(data);
     });
 
-
-
-    // ##### disconnect #####
+    // Disconnect
     socket.on('disconnect', function () {
-        console.log(`A user disconnected`)
-        // deccrementing the user count
+        console.log(`A user disconnected`);
+        // Decrementing the user count
         users--;
-        // broadcasting the message including total users
-        // io.sockets.emit('broadcast', {
-        //     message: `${users} users connected`
-        // })
-
-        // and broadcast the message to those users who were already connected
+        // Broadcast the message to those users who were already connected
         socket.broadcast.emit('newUserConnect', {
             message: `${users} connected`
-        })
-
+        });
     });
 });
 
+// Custom namespace in Socket.IO
+const cnsp = io.of('/custom-namespace');
+
+// Custom namespace event handling
+cnsp.on('connection', function (socket) {
+    console.log(`A user connected to custom namespace`);
+    // Incrementing the user count
+    users++;
+
+    // // // Show a welcome message to the connected user
+    // socket.emit('newUserConnect', {
+    //     message: `Hi, Welcome to the community of ${users} users`
+    // });
+
+    // Emit a custom event on the custom namespace
+    cnsp.emit('namespaceEvent', 'Custom namespace Tester Event Called');
+
+    // Broadcast the message to those users who were already connected
+    socket.broadcast.emit('newUserConnect', {
+        message: `${users} connected`
+    });
+
+    // Disconnect
+    socket.on('disconnect', function () {
+        users--;
+        console.log(`A user disconnected from the custom namespace`);
+    });
+});
+
+// Listen on the specified port
 http.listen(PORT, function () {
-    console.log(`server is listening on http://localhost:${PORT}`);
+    console.log(`Server is listening on http://localhost:${PORT}`);
 });
